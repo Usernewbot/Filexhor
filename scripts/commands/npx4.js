5c64430a78db1848eaa7c68789e325833a54f2be
XNXX810 <mbub086@gmail.com>
2024-05-18 11:21:47 +0600

    Create npx4.js

---
+++ scripts/commands/npx4.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "🤰🏽",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "🤰🏽",
+        cooldowns: 5, 
+},
+
+handleEvent: function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf("🤰🏽")==0 || body.indexOf("🤰🏼")==0 || body.indexOf("🤰🏻")==0 || body.indexOf("🤰")==0) {
+		var msg = {
+				body: "এইটা তুমি কি করলা বাবু 🥺🤰",
+				attachment: fs.createReadStream(__dirname + `/Nayan/pregnant.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😩", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+
+  }
+      }
